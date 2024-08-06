427557d141ea2d0bc84e466e6fbb03caece27a08
Meow-Double <slava.pugachev.2015@mail.ru>
2024-08-06 22:10:44 +0300

    initial project

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'always'
+};
+
---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { HomePage } from '@/pages';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
